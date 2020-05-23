361b3b3d28a647a11c0434bde4be8581ef6258fe
Полина Кузнецова <p.kuznetsova97@gmail.com>
2020-05-23 22:33:46 +0300

    assignment in progress

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,79 +1,168 @@
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+
     private int[][] grid;
-    private WeightedQuickUnionUF uf;
-    private boolean[][] isopen;
-    private int opensites;
+    private WeightedQuickUnionUF uf_top_bottom;
+    private WeightedQuickUnionUF uf_top;
+    private boolean[][] is_open;
+    private int opened_sites;
+    private int n;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("n must be bigger than 0");
         }
-        int newn = n + 2;
-        isopen = new boolean[newn][newn];
-        grid = new int[newn][newn];
-        int idx = 1;
-        for (int i = 1; i < newn - 1; i++) {
-            for (int j = 1; j < newn - 1; j++) {
+        this.n = n;
+        grid = new int[n][n];
+        is_open = new boolean[n][n];
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 grid[i][j] = idx;
-                isopen[i][j] = false;
+                is_open[i][j] = false;
                 idx++;
             }
         }
-        uf = new WeightedQuickUnionUF(n * n + 2);
-        for (int i = 1; i < newn - 1; i++) {
-            uf.union(0, grid[1][i]);
+        uf_top_bottom = new WeightedQuickUnionUF(n * n + 2); //uf instance with connection with top and bottom
+        for (int i = 0; i < n; i++) {
+            uf_top_bottom.union(n * n, grid[0][i]);
         }
-        for (int i = newn - 2; i >= 1; i--) {
-            uf.union(n * n + 1, grid[newn - 2][i]);
+        for (int i = n - 1; i >= 0; i--) {
+            uf_top_bottom.union(n * n + 1, grid[n - 1][i]);
+        }
+        uf_top = new WeightedQuickUnionUF(n * n + 1); // uf instance with connection only with top. All for correctness of isFull() method.
+        for (int i = 0; i < n; i++) {
+            uf_top_bottom.union(n * n, grid[0][i]);
         }
     }
 
-    private void validate(int row, int col) {
-        int n = grid.length - 2;
-        if (row < 1 || row > n || col < 1 || col > n) {
-            throw new IllegalArgumentException();
+        private void validate(int row, int col) {
+            if (row < 1 || row > n || col < 1 || col > n) {
+                throw new IllegalArgumentException("Indices must be between 1 and n");
+            }
         }
-    }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
-        int site = grid[row][col];
-        int[] neighbours = new int[4];
-        neighbours[0] = grid[row - 1][col];
-        neighbours[1] = grid[row][col + 1];
-        neighbours[2] = grid[row + 1][col];
-        neighbours[3] = grid[row][col - 1];
-        boolean[] opened = new boolean[4];
-        opened[0] = isOpen(row - 1, col);
-        opened[1] = isOpen(row, col + 1);
-        opened[2] = isOpen(row + 1, col);
-        opened[3] = isOpen(row, col - 1);
-        for (int i = 0; i < 4; i ++) {
-            if (neighbours[i] != 0 && opened[i]) {
-                uf.union(site, neighbours[i]);
+        int site = grid[row - 1][col - 1];
+        int cur_row = row - 1;
+        int cur_col = col - 1;
+        if (cur_row == 0) {
+            int neighbour3 = grid[cur_row + 1][cur_col];
+            if (isOpen(row + 1, col)) {
+                uf_top_bottom.union(site, neighbour3);
+                uf_top.union(site, neighbour3);
+            }
+            if (cur_col == 0) {
+                int neighbour2 = grid[cur_row][cur_col + 1];
+                if (isOpen(row, col + 1)) {
+                    uf_top_bottom.union(site, neighbour2);
+                    uf_top.union(site, neighbour2);
+                }
+            } else if (cur_col == n - 1) {
+                int neighbour4 = grid[cur_row][cur_col - 1];
+                if (isOpen(row, col - 1)) {
+                    uf_top_bottom.union(site, neighbour4);
+                    uf_top.union(site, neighbour4);
+                }
+            } else {
+                int neighbour2 = grid[cur_row][cur_col + 1];
+                if (isOpen(row, col + 1)) {
+                    uf_top_bottom.union(site, neighbour2);
+                    uf_top.union(site, neighbour2);
+                }
+                int neighbour4 = grid[cur_row][cur_col - 1];
+                if (isOpen(row, col - 1)) {
+                    uf_top_bottom.union(site, neighbour4);
+                    uf_top.union(site, neighbour4);
+                }
+            }
+        } else if (cur_row == n - 1) {
+            int neighbour1 = grid[cur_row - 1][cur_col];
+            if (isOpen(row - 1, col)) {
+                uf_top_bottom.union(site, neighbour1);
+                uf_top.union(site, neighbour1);
+            }
+            if (cur_col == 0) {
+                int neighbour2 = grid[cur_row][cur_col + 1];
+                if (isOpen(row, col + 1)) {
+                    uf_top_bottom.union(site, neighbour2);
+                    uf_top.union(site, neighbour2);
+                }
+            } else if (cur_col == n - 1) {
+                int neighbour4 = grid[cur_row][cur_col - 1];
+                if (isOpen(row, col - 1)) {
+                    uf_top_bottom.union(site, neighbour4);
+                    uf_top.union(site, neighbour4);
+                }
+            } else {
+                int neighbour2 = grid[cur_row][cur_col + 1];
+                if (isOpen(row, col + 1)) {
+                    uf_top_bottom.union(site, neighbour2);
+                    uf_top.union(site, neighbour2);
+                }
+                int neighbour4 = grid[cur_row][cur_col - 1];
+                if (isOpen(row, col - 1)) {
+                    uf_top_bottom.union(site, neighbour4);
+                    uf_top.union(site, neighbour4);
+                }
+            }
+        } else {
+            int neighbour1 = grid[cur_row - 1][cur_col];
+            if (isOpen(row - 1, col)) {
+                uf_top_bottom.union(site, neighbour1);
+                uf_top.union(site, neighbour1);
+            }
+            int neighbour3 = grid[cur_row + 1][cur_col];
+            if (isOpen(row + 1, col)) {
+                uf_top_bottom.union(site, neighbour3);
+                uf_top.union(site, neighbour3);
+            }
+            if (cur_col == 0) {
+                int neighbour2 = grid[cur_row][cur_col + 1];
+                if (isOpen(row, col + 1)) {
+                    uf_top_bottom.union(site, neighbour2);
+                    uf_top.union(site, neighbour2);
+                }
+            } else if (cur_col == n - 1) {
+                int neighbour4 = grid[cur_row][cur_col - 1];
+                if (isOpen(row, col - 1)) {
+                    uf_top_bottom.union(site, neighbour4);
+                    uf_top.union(site, neighbour4);
+                }
+            } else {
+                int neighbour2 = grid[cur_row][cur_col + 1];
+                if (isOpen(row, col + 1)) {
+                    uf_top_bottom.union(site, neighbour2);
+                    uf_top.union(site, neighbour2);
+                }
+                int neighbour4 = grid[cur_row][cur_col - 1];
+                if (isOpen(row, col - 1)) {
+                    uf_top_bottom.union(site, neighbour4);
+                    uf_top.union(site, neighbour4);
+                }
             }
         }
-        isopen[row][col] = true;
-        opensites++;
+        is_open[row - 1][col - 1] = true;
+        opened_sites++;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
-        return isopen[row][col];
+        return is_open[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
-        int site = grid[row][col];
-        int n = grid[0].length;
-        for (int i = 1; i < n - 1; i++) {
-            if (uf.find(site) == uf.find(grid[1][i])) {
+        int site = grid[row - 1][col - 1];
+        for (int i = 0; i < n; i++) {
+            if (uf_top.find(site) == uf_top.find(grid[0][i])) {
                 return true;
             }
         }
@@ -82,18 +171,17 @@ public class Percolation {
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return opensites;
+        return opened_sites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        int n = grid.length - 2;
-        return uf.find(0) == uf.find(n * n + 1);
+        return uf_top_bottom.find(n * n) == uf_top_bottom.find(n * n + 1);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation p = new Percolation(3);
+        Percolation p = new Percolation(10);
         for (int i = 0; i < p.grid.length; i++) {
             for (int j = 0; j < p.grid[0].length; j++) {
                 System.out.print(p.grid[i][j]);
@@ -101,21 +189,15 @@ public class Percolation {
             }
             System.out.println();
         }
-        System.out.println(p.isOpen(1, 1));
-        p.open(1, 2);
-        System.out.println(p.isOpen(1, 2));
-        p.open(2, 2);
-        System.out.println(p.isOpen(2, 2));
-        System.out.println(p.isFull(2, 2));
-        System.out.println(p.numberOfOpenSites());
-        p.open(3, 2);
-        p.open(1, 1);
-        p.open(2, 1);
-        System.out.println(p.isFull(2, 1));
-        System.out.println(p.isFull(2, 3));
-        System.out.println(p.isOpen(1, 3));
-        System.out.println(p.uf.find(1) + " " + p.uf.find(6));
-        System.out.println(p.numberOfOpenSites());
-        System.out.println(p.percolates());
+        while (!p.percolates()) {
+            int row = (int) (StdRandom.uniform() * 10) + 1;
+            int col = (int) (StdRandom.uniform() * 10) + 1;
+            if (p.isOpen(row, col)) {
+                continue;
+            }
+            p.open(row, col);
+        }
+        double x = (double) p.numberOfOpenSites() / 100;
+        System.out.println(x);
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,2 +1,57 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private int T;
+    private double[] x;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.T = trials;
+        Percolation p = new Percolation(n);
+        x = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            while (!p.percolates()) {
+                int row = (int) (StdRandom.uniform() * n) + 1;
+                int col = (int) (StdRandom.uniform() * n) + 1;
+                if (p.isOpen(row, col)) {
+                    continue;
+                }
+                p.open(row, col);
+            }
+            x[i] = (double) (p.numberOfOpenSites()) / (n * n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(x);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(x);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - (1.96 * stddev() / Math.sqrt(T));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + (1.96 * stddev() / Math.sqrt(T));
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, T);
+        System.out.println("mean = " + ps.mean());
+        System.out.println("stddev = " + ps.stddev());
+        System.out.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+    }
 }
